6b096aacdb82bb315a0cfb7da2fbf21eaca9efcf
optimus1010t <tanishqprasad1003@gmail.com>
2024-03-28 17:38:12 +0530

    added initial files

--- codes/main.c
+++ codes/main.c
@@ -5,34 +5,18 @@
 #include <signal.h>
 #include <errno.h>
 
-volatile sig_atomic_t got_usr1;
-
-void sigusr1_handler(int sig)
+void f(int n)
 {
-    got_usr1 = 1;
+    if (n  < 2){
+        printf("hello world\n");
+        exit(0);
+    }
+    if (fork()!=0) f(n-1);
+    if (fork()!=0) f(n-2);
 }
 
 int main(void)
 {
-        struct sigaction sa = {
-            .sa_handler = sigusr1_handler,
-            .sa_flags = 0, // or SA_RESTART
-            .sa_mask = 0,
-        };
-
-    got_usr1 = 0;
-
-    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(1);
-    }
-
-    while (!got_usr1) {
-        printf("PID %d: working hard...\n", getpid());
-        sleep(1);
-    }
-
-    printf("Done in by SIGUSR1!\n");
-
+    f(3);
     return 0;
 }
